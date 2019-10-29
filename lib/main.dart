5c68ca5e07369e26008644149f75b5a16901a49d
monis <monis5362413@gmail.com>
2019-10-29 10:46:15 +0800

    add _scanQR()

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,45 @@
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MaterialApp(
       home: HomePages(),
     ));
 
-class HomePages extends StatelessWidget {
+class HomePages extends StatefulWidget {
+  @override
+  _HomePagesState createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  String result = 'Hey there!';
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+      });
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          result = 'Camera permission was denied';
+        });
+      } else {
+        setState(() {
+          result = 'Unknow Error $e';
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = 'You pressed the back button before scanning anything';
+      });
+    } catch (e) {
+      setState(() {
+        result = 'Unknow Error $e';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,13 +48,13 @@ class HomePages extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          'Hey There!',
+          result,
           style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         icon: Icon(Icons.camera_alt),
-        onPressed: () {},
+        onPressed: _scanQR,
         label: Text('Scan'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
